a24556a297c52b19721eca092dbbe1061d434a92
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2024-04-15 10:42:32 +0300

    Deploy React app to GitHub Pages

---
+++ precache-manifest.6429fdc60da7be042c18e9ded6a45fdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a8452c6d6f253e75e47ac8b566a68a",
+    "url": "/React-Hacker-News/index.html"
+  },
+  {
+    "revision": "4e2e2d765f2488c67c2c",
+    "url": "/React-Hacker-News/static/css/main.8c244169.chunk.css"
+  },
+  {
+    "revision": "8116c3be2bbb50665cf0",
+    "url": "/React-Hacker-News/static/js/2.b5c32e4c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Hacker-News/static/js/2.b5c32e4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e2e2d765f2488c67c2c",
+    "url": "/React-Hacker-News/static/js/main.0cca6740.chunk.js"
+  },
+  {
+    "revision": "5da85e9175b316c44194",
+    "url": "/React-Hacker-News/static/js/runtime-main.fd289b27.js"
+  }
+]);
